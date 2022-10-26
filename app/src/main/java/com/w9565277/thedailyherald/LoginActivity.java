49352b6c4ad3716094f9418178386789cf78f1d3
safinchacko <w9565277@tees.ac.uk>
2022-10-26 01:11:26 +0100

    Sign up

--- app/src/main/java/com/w9565277/thedailyherald/LoginActivity.java
+++ app/src/main/java/com/w9565277/thedailyherald/LoginActivity.java
@@ -1,14 +1,62 @@
 package com.w9565277.thedailyherald;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.google_signin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn();
+            }
+        });
+    }
+
+    public void SignIn() {
+
+          Intent intent = gsc.getSignInIntent();
+          startActivityForResult(intent,100);
+    }
+
+    protected void OnActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                loadListingActivity();
+            } catch (ApiException e) {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void loadListingActivity() {
+        finish();
+        Intent intent = new Intent(getApplicationContext(), NewsListingActivity.class);
     }
 }
